2bda2006a5a6d208c40abe30baca238d3bdbcd7b
415 <a415@415demacbook-pro.local>
2017-01-18 15:10:39 +0800

    Initial Commit

---
+++ BridgePattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgePattern
+//
+//  Created by 415 on 17/1/18.
+//  Copyright © 2017年 415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
